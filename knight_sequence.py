14442ae97efb177a7c1f13387a181c44b8edc987
Niharika Gupta <niharikagupta1992@gmail.com>
2023-01-11 01:27:27 +0800

    V2 Commit

--- knight_sequence.py
+++ knight_sequence.py
@@ -47,4 +47,4 @@ if __name__ == '__main__':
     # First the key identity which makes the rest of the code easier to read.
     keyIdentity = [ {'A':0}, {'B':1}, {'C':2}, {'D':3}, {'E':4}, {'F':5}, {'G':6}, {'H':7}, {'I':8}, {'J':9}, {'K':10}, {'L':11}, {'M':12}, {'N':13}, {'O':14}, {'1':15}, {'2':16}, {'3' :17}]
     
-    print ("Total number of valid "+str(totalKeysInSequence)+ " key sequences are: " + str(dfs(range(len(keyIdentity)), totalKeysInSequence, allowedVowels)))# your code goes here
+    print ("Total number of valid "+str(totalKeysInSequence)+ " key sequences are: " + str(dfs(range(len(keyIdentity)), totalKeysInSequence, allowedVowels)))
